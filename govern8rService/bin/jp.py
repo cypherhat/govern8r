17bf7f9157295fa1fd4b467ddbee641d8bf7c74a
Jeff Ploughman <j.j.ploughman@gmail.com>
2015-12-22 18:07:14 -0500

    Code scraps

--- govern8rService/bin/jp.py
+++
@@ -1,4 +0,0 @@
-#!/Users/tssbi08/govern8r/virtualenvs/govern8rService/bin/python
-# EASY-INSTALL-SCRIPT: 'jmespath==0.9.0','jp.py'
-__requires__ = 'jmespath==0.9.0'
-__import__('pkg_resources').run_script('jmespath==0.9.0', 'jp.py')
--- govern8rService/bin/rst2xml.py
+++
@@ -1,4 +0,0 @@
-#!/Users/tssbi08/govern8r/virtualenvs/govern8rService/bin/python
-# EASY-INSTALL-SCRIPT: 'docutils==0.12','rst2xml.py'
-__requires__ = 'docutils==0.12'
-__import__('pkg_resources').run_script('docutils==0.12', 'rst2xml.py')
