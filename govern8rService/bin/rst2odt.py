137881a512945aa00c213bcf9d1a7792795366c3
Jeff Ploughman <j.j.ploughman@gmail.com>
2015-12-22 18:06:29 -0500

    Code scraps

---
+++ govern8rService/bin/rst2odt.py
@@ -0,0 +1,4 @@
+#!/Users/tssbi08/govern8r/virtualenvs/govern8rService/bin/python
+# EASY-INSTALL-SCRIPT: 'docutils==0.12','rst2odt.py'
+__requires__ = 'docutils==0.12'
+__import__('pkg_resources').run_script('docutils==0.12', 'rst2odt.py')
