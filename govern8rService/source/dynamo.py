241c63282808cb1f6d950cf212e9bd5c67209aee
Jeff Ploughman <j.j.ploughman@gmail.com>
2015-12-23 12:36:47 -0500

    Testing dynamoDB with boto3

--- govern8rService/source/dynamo.py
+++ govern8rService/source/dynamo.py
@@ -1,7 +1,7 @@
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://localhost:8000")
 
 
 table = dynamodb.create_table(
