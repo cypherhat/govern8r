bfbb958ae9c17222059664d3a813c29aca7845bb
Jeff Ploughman <j.j.ploughman@gmail.com>
2015-12-23 15:56:41 -0500

    More scraps

--- govern8rService/source/dynamo.py
+++ govern8rService/source/dynamo.py
@@ -4,28 +4,19 @@ import boto3
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://localhost:8000")
 
 
-table = dynamodb.create_table(
-    TableName='Movies',
+account_table = dynamodb.create_table(
+    TableName='Account',
     KeySchema=[
         {
-            'AttributeName': 'year',
-            'KeyType': 'HASH'  #Partition key
-        },
-        {
-            'AttributeName': 'title',
-            'KeyType': 'RANGE'  #Sort key
+            'AttributeName': 'email',
+            'KeyType': 'HASH'
         }
     ],
     AttributeDefinitions=[
         {
-            'AttributeName': 'year',
-            'AttributeType': 'N'
-        },
-        {
-            'AttributeName': 'title',
+            'AttributeName': 'email',
             'AttributeType': 'S'
-        },
-
+        }
     ],
     ProvisionedThroughput={
         'ReadCapacityUnits': 10,
@@ -33,4 +24,27 @@ table = dynamodb.create_table(
     }
 )
 
-print("Table status:", table.table_status)
+print("Account Table status:", account_table.table_status)
+
+notarization_table = dynamodb.create_table(
+    TableName='Notarization',
+    KeySchema=[
+        {
+            'AttributeName': 'digest',
+            'KeyType': 'HASH'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'digest',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+
+
+)
+print("Notarization Table status:", notarization_table.table_status)
