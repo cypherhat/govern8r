a783d718f518eea2b53472469c9be868a03ac76e
Jeff Ploughman <j.j.ploughman@gmail.com>
2015-12-24 15:18:57 -0500

    More scraps

--- govern8rClient/source/testsign.py
+++ govern8rClient/source/testsign.py
@@ -1,6 +1,8 @@
 
 from bitcoin.wallet import CBitcoinSecret, P2PKHBitcoinAddress
 from bitcoin.signmessage import BitcoinMessage, VerifyMessage, SignMessage
+import encrypt
+
 
 import requests
 
@@ -8,18 +10,22 @@ key = CBitcoinSecret("L4vB5fomsK8L95wQ7GFzvErYGht49JsCPJyJMHpB4xGM6xgi2jvG")
 address = P2PKHBitcoinAddress.from_pubkey(key.pub)  # "1F26pNMrywyZJdr22jErtKcjF8R3Ttt55G"
 message = "bitid://localhost:5000/callback?x=30f56bc022dde976&u=1"
 
-message = BitcoinMessage(message)
+btcmessage = BitcoinMessage(message)
 
-signature = SignMessage(key, message)
+print("\nClear: %s" % message)
+encrypted = encrypt.encrypt(key.pub, message)
+print("\nEncrypted: %s" % encrypted)
+
+decrypted = encrypt.decrypt("L4vB5fomsK8L95wQ7GFzvErYGht49JsCPJyJMHpB4xGM6xgi2jvG", encrypted)
+print("\nDecrypted: %s" % decrypted)
+
+signature = SignMessage(key, btcmessage)
 
 print(key, address)
 print("Address: %s" % address)
-print("Message: %s", message)
+print("Message: %s", btcmessage)
 print("\nSignature: %s" % signature)
-print("\nVerified: %s" % VerifyMessage(address, message, signature))
+print("\nVerified: %s" % VerifyMessage(address, btcmessage, signature))
 
 print("\nTo verify using bitcoin core;")
-print("`bitcoin-cli verifymessage %s \"%s\" \"%s\"`" % (address, signature.decode('ascii'), message))
-
-r = requests.get("http://localhost:5000/")
-print(r.content)
+print("`bitcoin-cli verifymessage %s \"%s\" \"%s\"`" % (address, signature.decode('ascii'), btcmessage))
