45769be817519c47b2b9b8ff2bbf80018b160543
Jeff Ploughman <none@none.com>
2015-12-29 14:18:57 -0500

    Refactor wallet

--- govern8rClient/testsign.py
+++ govern8rClient/testsign.py
@@ -1,52 +1,5 @@
 import encrypt
-from bitcoinlib.signmessage import BitcoinMessage, VerifyMessage, SignMessage
-from bitcoinlib.wallet import CBitcoinSecret, P2PKHBitcoinAddress
-import os
-import hashlib
 from wallet import NotaryWallet
-
-
-
-b58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-
-
-def base58encode(n):
-    result = ''
-    while n > 0:
-        result = b58[n % 58] + result
-        n /= 58
-    return result
-
-
-def countLeadingChars(s, ch):
-    count = 0
-    for c in s:
-        if c == ch:
-            count += 1
-        else:
-            break
-    return count
-
-
-
-def base256decode(s):
-    result = 0
-    for c in s:
-        result = result * 256 + ord(c)
-    return result
-
-
-def base58CheckEncode(version, payload):
-    s = chr(version) + payload
-    checksum = hashlib.sha256(hashlib.sha256(s).digest()).digest()[0:4]
-    result = s + checksum
-    leadingZeros = countLeadingChars(result, '\0')
-    return '1' * leadingZeros + base58encode(base256decode(result))
-
-
-def privateKeyToWif(key_hex):
-    return base58CheckEncode(0x80, key_hex.decode('hex'))
-
 
 wallet = NotaryWallet()
 
@@ -66,7 +19,5 @@ print("\nDecrypted: %s" % decrypted)
 
 signature = wallet.sign(message)
 
-btcmessage = BitcoinMessage(message)
 print("\nSignature: %s" % signature)
-print("\nVerified: %s" % VerifyMessage(wallet.get_bitcoin_address(), btcmessage, signature))
-print("\nVerified: %s" % wallet.verify(wallet.get_bitcoin_address(), message, signature))
+print("\nVerified: %s" % wallet.verify(message, signature))
