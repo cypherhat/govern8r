6cc470e6bfb924157497cd962aea80647e7454e8
Jeff Ploughman <none@none.com>
2016-01-01 10:06:07 -0500

    Create Notary class

--- govern8rClient/notary.py
+++ govern8rClient/notary.py
@@ -3,79 +3,72 @@ import argparse
 import requests
 import json
 from wallet import NotaryWallet
-from bitcoinlib.wallet import P2PKHBitcoinAddress
 from message import SecureMessage
 
-url='http://127.0.0.1:5000/govern8r'
+
+class Notary(object):
+    def __init__(self, notary_url):
+        self.notary_url = notary_url
+        self.wallet = NotaryWallet()
+        response = requests.get(self.notary_url+'/api/v1/pubkey')
+        data = response.json()
+        self.other_party_public_key = data['public_key']
+
+    def register_user(self, email):
+        secure_message = SecureMessage()
+        address = str(self.wallet.get_bitcoin_address())
+        message = {'public_key': self.wallet.get_public_key_hex(), 'email': email}
+        str_message = json.dumps(message)
+        payload = secure_message.create_secure_payload(self.other_party_public_key, str_message)
+        response = requests.put(self.notary_url+'/api/v1/account/' + address, data=payload)
+        print(response.status_code)
+
+    @staticmethod
+    def confirm_registration(confirmation_url):
+        response = requests.get(confirmation_url)
+        return response.status_code
+
+    def notarize_file(self, path_to_file):
+        print path_to_file
 
 
-def main() :
+def main():
+    notary_url = 'http://127.0.0.1:5000/govern8r'
+    notary = Notary(notary_url)
     parser = argparse.ArgumentParser()
     parser.add_argument("command", choices=['register', 'confirm', 'notary'], help="Name of the command.")
     parser.add_argument("-email", type=str, help="the email address of the registered user.")
     parser.add_argument("-file", type=file, help="Fully qualified name of the file to notarize.")
-    parser.add_argument("-confirmurl", type=str, help="Confirmation URL to confirm an account.")
+    parser.add_argument("-confirm_url", type=str, help="Confirmation URL to confirm an account.")
     args = parser.parse_args()
     command = args.command
     print command
     if command == "register":
-       print "running register command"
-       if( not args.email):
+        print "running register command"
+        if not args.email:
             print "register command needs email address"
-       else:
+        else:
             print args.email
-            register_user(args.email)
+            notary.register_user(args.email)
 
     elif command == "confirm":
         print "running confirm command"
-        if( not args.confirmurl):
+        if not args.confirm_url:
             print "confirm command needs url"
         else:
             print args.confirmurl
-            confirm_user(args.confirmurl)
+            Notary.confirm_registration(args.confirm_url)
     elif command == "notary":
 
         print "running notary command"
-        if( not args.file):
+        if not args.file:
             print "notary command needs file"
         else:
             print args.file
-            notarize_file(args.file)
+            notary.notarize_file(args.file)
     else:
         print "no command"
 
-
-def register_user(email):
-
-    wallet = NotaryWallet()
-    ## Test GET pubkey
-    req_pubkey = requests.get(url+'/api/v1/pubkey')
-    data = req_pubkey.json()
-    other_party_public_key = data['public_key']
-    print data['public_key']
-    secure_message = SecureMessage()
-    address = str(wallet.get_bitcoin_address())
-
-    ## Test POST account
-
-    print("\nWallet Public Key Hex %s" % wallet.get_public_key_hex())
-    print("\nWallet Public Key %s" % wallet.get_public_key())
-    address_from_hex = P2PKHBitcoinAddress.from_pubkey(wallet.get_public_key_hex().decode("hex"))
-    print("\nAddress From Hex %s" % address_from_hex)
-
-    registration_message = {'public_key': wallet.get_public_key_hex(), 'email': email}
-
-    registration_payload = secure_message.create_secure_payload(other_party_public_key, json.dumps(registration_message))
-    response = requests.put(url+'/api/v1/account/' + address, data=registration_payload)
-    print(response.status_code)
-
-
-def confirm_user(confirmation_url):
-    print confirmation_url
-
-
-def notarize_file(path_to_file):
-    print path_to_file
 
 if __name__ == "__main__":
     main()
